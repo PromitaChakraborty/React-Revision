ada3d2d1a2752be7f0334879d645597fabaa89e5
Promita Chakraborty <priyapromita@gmail.com>
2023-11-21 14:24:22 +0530

    Get React App

--- Nested.js
+++ Nested.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 Tag"),
-    React.createElement("h2", {}, "I am H2 Tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
+    React.createElement("h2", {}, "Hiiii Promita"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am H1 Tag"),
